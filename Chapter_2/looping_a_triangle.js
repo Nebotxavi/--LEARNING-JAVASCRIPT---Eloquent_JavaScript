175e1c82cf9ed973e68b92ad99340768bcfb6c23
NebotXavi <nebotxavi@gmail.com>
2019-06-19 00:12:09 +0100

    Triangle loop and FizzBuzz from chapter 2

---
+++ Chapter_2/looping_a_triangle.js
@@ -0,0 +1,14 @@
+/*
+Write a loop that makes seven calls to console.log to output the following triangle:
+#
+##
+###
+####
+#####
+######
+#######
+*/
+
+for (let h = '#'; h.length <= 7; h += '#') {
+    console.log(h)
+}
