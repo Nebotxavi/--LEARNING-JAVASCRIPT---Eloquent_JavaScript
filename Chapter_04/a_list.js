c71f44dd277684f8f252b26c8c8815aee97cc277
NebotXavi <46298544+Nebotxavi@users.noreply.github.com>
2019-07-01 19:57:41 +0100

    Update a_list.js

--- Chapter_04/a_list.js
+++ Chapter_04/a_list.js
@@ -9,58 +9,32 @@ element) or undefined when there is no such element.
 If you haven’t already, also write a recursive version of nth .
 */
 
-function arrayToList(arr) {
+const arrayToList = arr => {
     let list = null;
-    for (let i = arr.length - 1; i >= 0; i--) {
-        list = { value: arr[i], rest: list };
+    for (let num = arr.length - 1; num >= 0; num--) {
+        list = { value: arr[num], rest: list };
     }
     return list;
 }
 
-function listToArray(list) {
+const listToArray = list => {
     let arr = [];
-    for (let i = list; i; i = i['rest']) {
-        arr.push(i['value']);
-    }
+    for (let i = list; i; i = i['rest']) arr.push(i['value']);
     return arr;
 }
 
-function prepend(value_item, list) {
-    new_list = {
-        value: value_item,
-        rest: list
-    };
-    return new_list;
+const prepend = (value, rest) => {
+    let newList = { value, rest };
+    return newList;
 }
 
-// Option proposed in the book:
-
-function prepend2(value, list) {
-    return { value, rest: list };
-}
-
-function nth(list, n) {
-    nth = 0;
-    a = 0;
-    for (let i = list; a <= n; i = i['rest']) {
-        nth = (i['value']);
-        a++;
-    }
-    return nth;
-}
-
-// Option proposed in the book:
-
-function nth2(list, n) {
-    if (!list) return undefined;
-    else if (n == 0) return list.value;
-    else return nth2(list.rest, n - 1);
+const nth = (list, n) => {
+    if (n == 0) return list['value'];
+    else return (nth(list['rest'], n - 1));
 }
 
 console.log(arrayToList([10, 20]));
 // → {value: 10, rest: {value: 20, rest: null}}
-console.log(arrayToList([10, 20, 30]));
-// → {value: 10, rest: {value: 20, rest: {value: 30, rest: null}}}
 console.log(listToArray(arrayToList([10, 20, 30])));
 // → [10, 20, 30]
 console.log(prepend(10, prepend(20, null)));
